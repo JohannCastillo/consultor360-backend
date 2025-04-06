5f1a79961e6d284ab83001e0e49e013bd3edf067
JohannCastillo <johannjco15022@gmail.com>
2025-04-05 22:25:52 -0500

    chore: add config for deployment on render

--- backend/settings.py
+++ backend/settings.py
@@ -98,7 +98,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
