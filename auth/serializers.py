80e0d0c791f571b219e73dc1fa49cb0d57886c99
JohannCastillo <johannjco15022@gmail.com>
2025-04-05 20:55:21 -0500

    feat: add validation by existing username in UserSerializer

--- auth/serializers.py
+++ auth/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 import base64
 
+
 class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=False)
@@ -10,20 +11,27 @@ class UserSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(required=False)
     password = serializers.CharField(write_only=True)
     token = serializers.SerializerMethodField()
-    
+
     class Meta(object):
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'token']
+        fields = ['id', 'username', 'first_name',
+                  'last_name', 'email', 'password', 'token']
 
     def get_token(self, obj):
         token = Token.objects.get_or_create(user=obj)
         encoded_token = base64.b64encode(token[0].key.encode())
         return encoded_token
-    
+
     def create(self, validated_data):
+        # validar que el usuario no exista
+        if User.objects.filter(username=validated_data['username']).exists():
+            raise serializers.ValidationError(
+                "El nombre de usuario ya se encuentra registrado.")
+
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
